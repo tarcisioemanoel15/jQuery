bd6d918828ade5b37cc0c149920b4ae64a75dd3d
Tarcisio <caninosbrancos10031@gmail.com>
2022-07-17 12:42:08 -0300

    finalizado

---
+++ ex2Puro/imgLup.js
@@ -0,0 +1,8 @@
+
+const contain = document.querySelector('.contain');
+const li = document.createElement('li');
+console.log(contain)
+
+li.innerHTML = ' <img src="../img/1.jpg" alt="1">';
+contain: contain.appendChild(li);
+
